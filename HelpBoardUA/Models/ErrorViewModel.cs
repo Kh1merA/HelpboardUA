ad010acc2ab4c0524a748d0c3a2c9342543abc34
antonPehotin <pehotin.anton@gmail.com>
2023-05-18 23:38:21 +0300

    first commit

---
+++ HelpBoardUA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelpBoardUA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
