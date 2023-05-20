110f4ef99c5340855c9961376fc441e46ddc393c
antonPehotin <pehotin.anton@gmail.com>
2023-05-20 23:02:49 +0300

    added Offer class

---
+++ HelpBoardUA/Models/Offer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace HelpBoardUA.Models
+{
+    public class Offer
+    {
+        private string title {  get; set; }
+        private string subtitle {  get; set; }
+        private string description {  get; set; }
+        
+        //ENUM?
+        //private type description {  get; set; }
+
+        private string area { get; set; }
+        private string city { get; set; }
+        private string address { get; set; }
+        private DateTime startDateTime { get; set; }
+        private DateTime finishDateTime { get; set; }
+        private Organization organizationName { get; set; }
+        private ArrayList daysForRecieve { get; set; }
+
+        private Dictionary<Client, DateTime> queue { get; set; }
+    }
+}
