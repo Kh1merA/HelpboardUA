27c5100d8a00598c9a3eb23948ccca8e3764fb4e
KPas13 <kpasenko64@gmail.com>
2023-05-21 16:59:28 +0300

    Create ForgotPass.cs
    create controller for forgotpass page

---
+++ HelpBoardUA/Controllers/ForgotPass.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelpBoardUA.Controllers
+{
+    public class ForgotPass : Controller
+    {
+        public IActionResult forgotPassPage()
+        {
+            return View();
+        }
+    }
+}
