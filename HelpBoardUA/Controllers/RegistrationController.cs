33b280f2680a851a7924b02983fbefc17926b4aa
KPas13 <kpasenko64@gmail.com>
2023-05-21 16:59:41 +0300

    Create RegistrationController.cs
    create controller for registration page

---
+++ HelpBoardUA/Controllers/RegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelpBoardUA.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult regPage()
+        {
+            return View();
+        }
+    }
+}
