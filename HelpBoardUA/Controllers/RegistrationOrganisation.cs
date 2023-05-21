599b0a86dfd00feb772eb552d525524bce7dc030
KPas13 <kpasenko64@gmail.com>
2023-05-21 16:59:58 +0300

    Create RegistrationOrganisation.cs
    create controller for registration of organisation page

---
+++ HelpBoardUA/Controllers/RegistrationOrganisation.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelpBoardUA.Controllers
+{
+    public class RegistrationOrganisation : Controller
+    {
+        public IActionResult regOrganisationPage()
+        {
+            return View();
+        }
+    }
+}
