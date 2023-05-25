4a9f3a13a31ab03cb6909faf3ff0447b9ffb72b0
Anika-02 <103873619+Anika-02@users.noreply.github.com>
2023-05-25 22:23:51 +0300

    Create EditUserProfileController.cs
    Create EditUserProfileController

---
+++ HelpBoardUA/Controllers/EditUserProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelpBoardUA.Controllers
+{
+	public class EditUserProfileController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
