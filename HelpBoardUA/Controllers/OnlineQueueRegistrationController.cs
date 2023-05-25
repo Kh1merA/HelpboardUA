d4350b40c7c17eaf2f1ed1daf8efe474f1ecf394
Anika-02 <103873619+Anika-02@users.noreply.github.com>
2023-05-26 01:55:51 +0300

    Create OnlineQueueRegistrationController.cs
    Create controller for online queue

---
+++ HelpBoardUA/Controllers/OnlineQueueRegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelpBoardUA.Controllers
+{
+	public class OnlineQueueRegistrationController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
