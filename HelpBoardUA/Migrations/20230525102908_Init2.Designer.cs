17d30dd87e5443f2635bdd71c04ba74e287c9b73
DavidKoshel <dowak1ne@gmail.com>
2023-05-25 15:05:59 +0300

    Registration is done

--- HelpBoardUA/Migrations/20230525102908_Init2.Designer.cs
+++ HelpBoardUA/Migrations/20230525102908_Init2.Designer.cs
@@ -4,6 +4,7 @@ using HelpBoardUA.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HelpBoardUA.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230525102908_Init2")]
+    partial class Init2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
