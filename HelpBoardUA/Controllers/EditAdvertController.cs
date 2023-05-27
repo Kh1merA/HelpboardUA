e86107c7b7492d59284182da25b21e4869410b4f
KPas13 <kpasenko64@gmail.com>
2023-05-27 22:34:31 +0300

    Create EditAdvertController.cs
    Add controller for EditAdvert Page

---
+++ HelpBoardUA/Controllers/EditAdvertController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelpBoardUA.Controllers
+{
+    public class EditAdvertController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
