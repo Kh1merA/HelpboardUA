086f6f83a04c75e0a3b15fc56dd5fb24b64277f8
antonPehotin <pehotin.anton@gmail.com>
2023-05-27 12:05:31 +0300

    added links to new pages, added maprazorpages

--- HelpBoardUA/Program.cs
+++ HelpBoardUA/Program.cs
@@ -44,6 +44,8 @@ app.UseRouting();
 app.UseAuthorization();
 app.UseAuthentication();
 
+app.MapRazorPages();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
