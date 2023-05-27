23ce920c9d07e62dd5ee33072b7ecb3889fa0496
antonPehotin <pehotin.anton@gmail.com>
2023-05-27 12:31:53 +0300

    added roles in Program.cs

--- HelpBoardUA/Program.cs
+++ HelpBoardUA/Program.cs
@@ -6,54 +6,78 @@ using System;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-string connection = builder.Configuration.GetConnectionString("HelpboardUAConnectionString");
-
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
-
-builder.Services.AddRazorPages();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<AppDbContext>();
-
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.Cookie.Name = "MyCookie";
-        options.LoginPath = "/Authorization/Login"; //login method path
-    });
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        string connection = builder.Configuration.GetConnectionString("HelpboardUAConnectionString");
+
+        builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+
+        builder.Services.AddRazorPages();
+
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>();
+
+        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.Cookie.Name = "MyCookie";
+                options.LoginPath = "/Authorization/Login"; //login method path
+            });
+
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+        app.UseAuthentication();
+
+        app.MapRazorPages();
+
+        app.UseEndpoints(endpoins =>
+            endpoins.MapRazorPages()
+        );
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+        //adding roles
+        using (var scope = app.Services.CreateScope())
+        {
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var roles = new[] { "Admin", "Organization", "Client" };
+
+            foreach (var role in roles)
+            {
+                if(!await roleManager.RoleExistsAsync(role)) 
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-app.UseAuthentication();
-
-app.MapRazorPages();
-
-app.UseEndpoints(endpoins =>
-    endpoins.MapRazorPages()
-);
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
