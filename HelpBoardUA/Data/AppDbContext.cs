3dcd659bcc7adde386d6e642a0bedb34ec1a2102
antonPehotin <pehotin.anton@gmail.com>
2023-05-27 14:10:27 +0300

    added entity configuration for client

--- HelpBoardUA/Data/AppDbContext.cs
+++ HelpBoardUA/Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using HelpBoardUA.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System.Data;
 
 namespace HelpBoardUA.Data
@@ -16,5 +17,24 @@ namespace HelpBoardUA.Data
         public DbSet<News> News { get; set; }
         public DbSet<Offer> Offers { get; set; }
         public DbSet<Organization> Organizations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new ClientEntityConfiguration());
+        }
+    }
+
+    public class ClientEntityConfiguration : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.Property(c => c.LastName).HasMaxLength(40);
+            builder.Property(c => c.FirstName).HasMaxLength(40);
+            builder.Property(c => c.Patronymic).HasMaxLength(40);
+            builder.Property(c => c.Sex);
+            builder.Property(c => c.Birth);
+        }
     }
 }
