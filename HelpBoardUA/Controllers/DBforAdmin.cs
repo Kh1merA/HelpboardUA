e6827e06f4febb10e6b72021c0b38d1f92eb9b01
Anika-02 <103873619+Anika-02@users.noreply.github.com>
2023-05-27 19:42:11 +0300

    Create DBforAdmin.cs
    Create controller for admin`s db

---
+++ HelpBoardUA/Controllers/DBforAdmin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelpBoardUA.Controllers
+{
+	public class DBforAdmin : Controller
+	{
+		public IActionResult UsersDB()
+		{
+			return View();
+		}
+
+		public IActionResult OrganizationsDB()
+		{
+			return View();
+		}
+	}
+}
