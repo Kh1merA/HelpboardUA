133fa91ad89008d37824da99cfba303f206e38d6
Kh1merA <dr.avtomonova28@gmail.com>
2023-05-27 16:54:32 +0300

    Create OrganizationPageController.cs
    create organization page for clients controller

---
+++ HelpBoardUA/Controllers/OrganizationPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelpBoardUA.Controllers
+{
+	public class OrganizationPageController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
