6c1c4aa09e9c80791a41ad36ff7932d39c442c26
antonPehotin <pehotin.anton@gmail.com>
2023-05-28 20:36:20 +0300

    edited layout

--- HelpBoardUA/Program.cs
+++ HelpBoardUA/Program.cs
@@ -6,50 +6,60 @@ using System;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-string connection = builder.Configuration.GetConnectionString("HelpboardUAConnectionString");
-
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
-
-
-//added automaticaly with area....
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<AppDbContext>();
-
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.Cookie.Name = "MyCookie";
-        options.LoginPath = "/Authorization/Login"; //login method path
-    });
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        string connection = builder.Configuration.GetConnectionString("HelpboardUAConnectionString");
+
+        builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+
+
+        //added automaticaly with area....
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            .AddEntityFrameworkStores<AppDbContext>();
+
+       
+        /*
+         * log in doesnt work with this code(
+         * 
+        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.Cookie.Name = "MyCookie";
+                options.LoginPath = "/Authorization/Login"; //login method path
+            });
+        */
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapRazorPages();
+
+        app.UseAuthorization();
+        app.UseAuthentication();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapRazorPages();
-
-app.UseAuthorization();
-app.UseAuthentication();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
