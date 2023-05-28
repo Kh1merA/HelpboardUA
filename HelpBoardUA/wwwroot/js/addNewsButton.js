f028a073be211a996d3bc052fa170fa959c4f54e
KPas13 <kpasenko64@gmail.com>
2023-05-29 00:59:56 +0300

    Update addNewsButton.js
    Update js code

--- HelpBoardUA/wwwroot/js/addNewsButton.js
+++ HelpBoardUA/wwwroot/js/addNewsButton.js
@@ -8,11 +8,18 @@
     });
 
     uploadFile.addEventListener("change", function () {
-        if (uploadFile.value) {
-            uploadText.innerText = uploadFile.value.match(/[\/\\]([\w\d\s\.\-(\)]+)$/)[1];
-        }
-        else {
+        const fileName = uploadFile.value;
+        const validExtensions = ["jpg", "jpeg", "png", "svg"];
+        const fileExtension = fileName.split(".").pop().toLowerCase();
+
+        if (!fileName) {
+
             uploadText.innerText = "Файл не обрано";
+        } else if (validExtensions.includes(fileExtension)) {
+            uploadText.innerText = fileName.match(/[\/\\]([\w\d\s\.\-(\)]+)$/)[1];
+        } else {
+            uploadFile.value = null;
+            uploadText.innerText = "Файл має непідтримуваний формат";
         }
     });
 
@@ -25,11 +32,20 @@
     });
 
     uploadFile2.addEventListener("change", function () {
-        if (uploadFile2.value) {
-            uploadText2.innerText = uploadFile2.value.match(/[\/\\]([\w\d\s\.\-(\)]+)$/)[1];
-        }
-        else {
+        const fileName2 = uploadFile2.value;
+        const validExtensions2 = ["jpg", "jpeg", "png", "svg"];
+        const fileExtension2 = fileName2.split(".").pop().toLowerCase();
+
+        if (!fileName2) {
+
             uploadText2.innerText = "Файл не обрано";
+        } else if (validExtensions2.includes(fileExtension2)) {
+            uploadText2.innerText = fileName2.match(/[\/\\]([\w\d\s\.\-(\)]+)$/)[1];
+        } else {
+            uploadFile2.value = null;
+            uploadText2.innerText = "Файл має непідтримуваний формат";
         }
     });
 }
+
+
