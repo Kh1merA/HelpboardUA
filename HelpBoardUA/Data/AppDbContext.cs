6e549ef45aa573b3cb249bee079f93d529de64ea
antonPehotin <pehotin.anton@gmail.com>
2023-05-29 20:44:11 +0300

    user info in userProfile

--- HelpBoardUA/Data/AppDbContext.cs
+++ HelpBoardUA/Data/AppDbContext.cs
@@ -11,12 +11,12 @@ namespace HelpBoardUA.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        //all db tables
+        //adb tables
         public DbSet<Client> Clients{ get; set; }
         public DbSet<News> News { get; set; }
         public DbSet<Offer> Offers { get; set; }
         public DbSet<Organization> Organizations { get; set; }
 
-        // need to add configs
+        // need to add configs?
     }
 }
