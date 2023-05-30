c50676fd2b1ed1509ad86eb01a9d9179e4db469e
antonPehotin <pehotin.anton@gmail.com>
2023-05-30 15:50:15 +0300

    news and offer lists in orgProfile

--- HelpBoardUA/Controllers/OrganizationProfileController.cs
+++ HelpBoardUA/Controllers/OrganizationProfileController.cs
@@ -3,21 +3,29 @@ using HelpBoardUA.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelpBoardUA.Controllers
 {
 	public class OrganizationProfileController : Controller
 	{
         private readonly UserManager<IdentityUser> _userManager;
+		private readonly AppDbContext _appDbContext;
 
-        public OrganizationProfileController(UserManager<IdentityUser> userManager)
+        public OrganizationProfileController(
+            UserManager<IdentityUser> userManager,
+            AppDbContext appDbContext)
         {
             _userManager = userManager;
+            _appDbContext = appDbContext;
         }
 
-        public async Task<IActionResult> IndexAsync()
+        public async Task<IActionResult> Index()
 		{
-            Organization organization = (Organization) await _userManager.GetUserAsync(User);
+
+			Organization organization = (Organization) await _userManager.GetUserAsync(User);
+            var orgId = _userManager.GetUserId(User);
+
             var name = organization.Name;
             var location = organization.Location;
             var phone = organization.PhoneNumber;
@@ -28,7 +36,18 @@ namespace HelpBoardUA.Controllers
             ViewBag.Phone = phone;
             ViewBag.Email = email;
 
-            return View();
+
+            var newsList = await _appDbContext.News.Where(n => n.OrganizationId == orgId).ToListAsync();
+            var offerList = await _appDbContext.Offers.Where(n => n.OrganizationId == orgId).ToListAsync();
+
+			var model = new OrganizationProfileViewModel
+			{
+				//Organization = organization,
+				NewsList = newsList,
+				OfferList = offerList
+			};
+
+			return View(model);
 		}
 	}
 }
---
+++ HelpBoardUA/Models/OrganizationProfileViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HelpBoardUA.Models
+{
+	public class OrganizationProfileViewModel
+	{
+		//public Organization Organization { get; set; } начебто не потрібно
+		public List<News> NewsList { get; set; }
+		public List<Offer> OfferList { get; set; }
+	}
+}
