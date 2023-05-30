30f00f19c869c2248d80843441a465f48421f50c
antonPehotin <pehotin.anton@gmail.com>
2023-05-30 16:55:28 +0300

    adding offers works without images and date)

--- HelpBoardUA/Models/AddNewOfferModel.cs
+++ HelpBoardUA/Models/AddNewOfferModel.cs
@@ -2,16 +2,16 @@
 {
 	public class AddNewOfferModel
 	{
-		public string Title { get; set; }
-		public string Subtitle { get; set; }
-		public string Description { get; set; }
-		public string OfferType { get; set; }
-		public string HelpType { get; set; }
-		public int NumberOfAid { get; set; }
-		public string Area { get; set; }
-		public string City { get; set; }
-		public string Address { get; set; }
-		public DateTime StartDateTime { get; set; }
-		public DateTime FinishDateTime { get; set; }
+		public string Title {  get; set; }
+        public string Subtitle {  get; set; }
+		public string Description {  get; set; }
+        public string OfferType { get; set; } 
+        public string HelpType { get; set; } 
+        public int NumberOfAid{ get; set; } 
+        public string Area { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime FinishDateTime { get; set; }
 	}
 }
