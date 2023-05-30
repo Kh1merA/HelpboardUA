9aae08263ed9a8350f036c285d4e7d54105783c0
antonPehotin <pehotin.anton@gmail.com>
2023-05-31 02:44:24 +0300

    offers editing without dates and days

--- HelpBoardUA/Controllers/EditAdvertController.cs
+++ HelpBoardUA/Controllers/EditAdvertController.cs
@@ -1,12 +1,77 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HelpBoardUA.Data;
+using HelpBoardUA.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelpBoardUA.Controllers
 {
     public class EditAdvertController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppDbContext _appDbContext;
+
+        public EditAdvertController(
+            UserManager<IdentityUser> userManager,
+            AppDbContext appDbContext)
         {
+            _userManager = userManager;
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(Guid id)
+        {
+            var thisOffer = await _appDbContext.Offers.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (thisOffer != null)
+            {
+                var viewModel = new EditAdvertViewModel()
+                {
+                    Id = id,
+                    Title = thisOffer.Title,
+                    Subtitle = thisOffer.Subtitle,
+                    Description = thisOffer.Description,
+                    OfferType = thisOffer.OfferType,
+                    HelpType = thisOffer.HelpType,
+                    NumberOfAid = thisOffer.NumberOfAid,
+                    Area = thisOffer.Area,
+                    City = thisOffer.City,
+                    Address = thisOffer.Address,
+                    StartDateTime = thisOffer.StartDateTime,
+                    FinishDateTime = thisOffer.FinishDateTime
+                };
+                return await Task.Run(() => View(viewModel));
+            }
             return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditAdvertViewModel model)
+        {
+            var offer = await _appDbContext.Offers.FindAsync(model.Id);
+
+            if (offer != null)
+            {
+                offer.Title = model.Title;
+                offer.Subtitle = model.Subtitle;
+                offer.Description = model.Description;
+                offer.OfferType = model.OfferType;  
+                offer.HelpType = model.HelpType;
+                offer.NumberOfAid = model.NumberOfAid;
+                offer.Area = model.Area;
+                offer.City = model.City;
+                offer.Address = model.Address;
+                offer.StartDateTime = model.StartDateTime;
+                offer.FinishDateTime = model.FinishDateTime;
+
+                await _appDbContext.SaveChangesAsync();
+
+                return LocalRedirect("~/OrganizationProfile/Index");
+            }
+
+            return LocalRedirect("~/OrganizationProfile/Index");
         }
     }
 }
---
+++ HelpBoardUA/Models/EditAdvertViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace HelpBoardUA.Models
+{
+    public class EditAdvertViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description { get; set; }
+        public string OfferType { get; set; }
+        public string HelpType { get; set; }
+        public int NumberOfAid { get; set; }
+        public string Area { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime FinishDateTime { get; set; }
+    }
+}
