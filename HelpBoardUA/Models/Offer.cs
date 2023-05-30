4395fe2aa9ed0a95c8d704019d5975b8b9c8ae1d
antonPehotin <pehotin.anton@gmail.com>
2023-05-30 16:02:09 +0300

    edited props for offer class, created AddNewOfferModel

--- HelpBoardUA/Models/Offer.cs
+++ HelpBoardUA/Models/Offer.cs
@@ -7,16 +7,19 @@ namespace HelpBoardUA.Models
         public Guid Id { get; set; }
         public string Title {  get; set; }
         public string Subtitle {  get; set; }
-
-		//ENUM?
 		public string Description {  get; set; }
+        public string OfferType { get; set; } 
+        public string HelpType { get; set; } 
+        public int NumberOfAid{ get; set; } 
         public string Area { get; set; }
         public string City { get; set; }
         public string Address { get; set; }
         public DateTime StartDateTime { get; set; }
         public DateTime FinishDateTime { get; set; }
-        public string OrganizationId { get; set; }
-		public ICollection<DayForRecieve> DaysForRecieve { get; set; }
-		public ICollection<OfferClient> OfferClients { get; set; }
+        
+		public ICollection<DayForRecieve> DaysForRecieve { get; set; } // <---- how???
+		public ICollection<OfferClient> OfferClients { get; set; } // <---- how???
+
+		public string OrganizationId { get; set; }
 	}
 }
