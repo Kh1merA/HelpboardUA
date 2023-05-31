342e609645769f46e76ed0d1f409db33acd358e6
DavidKoshel <dowak1ne@gmail.com>
2023-05-31 20:35:59 +0300

    created a filtration and viewing of humanitarian offers

--- HelpBoardUA/Controllers/HumanitarianController.cs
+++ HelpBoardUA/Controllers/HumanitarianController.cs
@@ -1,22 +1,43 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HelpBoardUA.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelpBoardUA.Controllers
 {
-    public class HumanitarianController : Controller
-    {    
-        public IActionResult Index()
-        {
-            return View();
-        }
+	public class HumanitarianController : Controller
+	{
+		private readonly AppDbContext appDbContext;
+		public HumanitarianController(AppDbContext appDbContext)
+		{
+			this.appDbContext = appDbContext;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Index(string? HelpType)
+		{
+			var offers = await appDbContext.Offers.ToListAsync();
+
+			if (HelpType == "kitchenHelp")		{ ViewBag.HelpType = "kitchenHelp"; }
+			if (HelpType == "childrenHelp")		{ ViewBag.HelpType = "childrenHelp"; }
+			if (HelpType == "bedLinen")			{ ViewBag.HelpType = "bedLinen"; }
+			if (HelpType == "animalsHelp")		{ ViewBag.HelpType = "animalsHelp"; }
+			if (HelpType == "medicalHelp")		{ ViewBag.HelpType = "medicalHelp"; }
+			if (HelpType == "foodHelp")			{ ViewBag.HelpType = "foodHelp"; }
+			if (HelpType == "informationHelp")	{ ViewBag.HelpType = "informationHelp"; }
+			if (HelpType == "hygieneHelp")		{ ViewBag.HelpType = "hygieneHelp"; }
+			if (HelpType == "moneyHelp")		{ ViewBag.HelpType = "moneyHelp"; }
+
+			return View(offers);
+		}
 
 		public IActionResult HumanitarianPage()
 		{
 			return View();
 		}
 
-        public IActionResult HumanPageForOrg() 
-        {
+		public IActionResult HumanPageForOrg()
+		{
 			return View();
 		}
 
@@ -26,4 +47,5 @@ namespace HelpBoardUA.Controllers
 		}
 
 	}
+	
 }
--- HelpBoardUA/Models/Organization.cs
+++ HelpBoardUA/Models/Organization.cs
@@ -7,6 +7,8 @@ namespace HelpBoardUA.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
+
+
         //public string Tel { get; set; }
         //public string Email { get; set; }
     }
