88bf8f27f522b1adc65f4b3e093d907fbff3c559
DavidKoshel <dowak1ne@gmail.com>
2023-05-31 16:37:13 +0300

    did a confirmation of Vpo-status with images

--- HelpBoardUA/Controllers/ConfirmationVPOController.cs
+++ HelpBoardUA/Controllers/ConfirmationVPOController.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HelpBoardUA.Data;
+using HelpBoardUA.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelpBoardUA.Controllers
 {
 	public class ConfirmationVPOController : Controller
 	{
-		public IActionResult Index()
+		private readonly AppDbContext appDbContext;
+
+		public ConfirmationVPOController (AppDbContext appDbContext)
 		{
-			return View();
+			this.appDbContext = appDbContext;
 		}
-	}
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			
+			var users = await appDbContext.Clients.ToListAsync();
+			return View(users);
+		}
+
+        [HttpPost]
+        public async Task<IActionResult> Confirm(string userName)
+        {
+            var client = await appDbContext.Clients.FirstOrDefaultAsync(c => c.UserName == userName);
+			if (client != null) 
+			{
+                client.VPO_Status = 2;
+                await appDbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Cancel(string userName)
+        {
+            var client = await appDbContext.Clients.FirstOrDefaultAsync(c => c.UserName == userName);
+            if (client != null)
+            {
+                client.VPO_Status = 0;
+                await appDbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
 }
