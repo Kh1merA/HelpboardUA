058d86b5da85a8c58eb062eb207771672240fb81
antonPehotin <pehotin.anton@gmail.com>
2023-06-02 00:43:11 +0300

    sorry

--- HelpBoardUA/Models/OfferClient.cs
+++ HelpBoardUA/Models/OfferClient.cs
@@ -4,10 +4,7 @@
 	{
 		public Guid Id { get; set; }
 		public int OfferId { get; set; }
-<<<<<<< HEAD
-		//public Offer Offer { get; set; }
-=======
->>>>>>> e2335b79e8f8cbcd517b5f0e7785bbe340635782
+
 		public int ClientId { get; set; }
 		public DateTime Date { get; set; }
 	}
