294f7d63b8a34b4eb04dabf62a59cc7308058ea6
KPas13 <kpasenko64@gmail.com>
2023-06-02 15:07:38 +0300

    Update editAdvertButtons.js
    Update js code

--- HelpBoardUA/wwwroot/js/editAdvertButtons.js
+++ HelpBoardUA/wwwroot/js/editAdvertButtons.js
@@ -1,25 +1,25 @@
-﻿window.onload = () => {
-    const uploadFile = document.getElementById("upload-file");
-    const uploadBtn = document.getElementById("upload-btn");
-    const uploadText = document.getElementById("upload-text");
+﻿//window.onload = () => {
+//    const uploadFile = document.getElementById("upload-file");
+//    const uploadBtn = document.getElementById("upload-btn");
+//    const uploadText = document.getElementById("upload-text");
 
-    uploadBtn.addEventListener("click", function () {
-        uploadFile.click();
-    });
+//    uploadBtn.addEventListener("click", function () {
+//        uploadFile.click();
+//    });
 
-    uploadFile.addEventListener("change", function () {
-        const fileName = uploadFile.value;
-        const validExtensions = ["jpg", "jpeg", "png", "svg"];
-        const fileExtension = fileName.split(".").pop().toLowerCase();
+//    uploadFile.addEventListener("change", function () {
+//        const fileName = uploadFile.value;
+//        const validExtensions = ["jpg", "jpeg", "png", "svg"];
+//        const fileExtension = fileName.split(".").pop().toLowerCase();
 
-        if (!fileName) {
+//        if (!fileName) {
 
-            uploadText.innerText = "Файл не обрано";
-        } else if (validExtensions.includes(fileExtension)) {
-            uploadText.innerText = fileName.match(/[\/\\]([\w\d\s\.\-(\)]+)$/)[1];
-        } else {
-            uploadFile.value = null;
-            uploadText.innerText = "Файл має непідтримуваний формат";
-        }
-    });
-}
+//            uploadText.innerText = "Файл не обрано";
+//        } else if (validExtensions.includes(fileExtension)) {
+//            uploadText.innerText = fileName.match(/[\/\\]([\w\d\s\.\-(\)]+)$/)[1];
+//        } else {
+//            uploadFile.value = null;
+//            uploadText.innerText = "Файл має непідтримуваний формат";
+//        }
+//    });
+//}
