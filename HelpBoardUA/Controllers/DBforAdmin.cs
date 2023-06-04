15d4525fd4e253f8caab9f065a6e87f3ca4da65d
DavidKoshel <dowak1ne@gmail.com>
2023-06-04 19:41:19 +0300

    added search in Org and User DB for Admin

--- HelpBoardUA/Controllers/DBforAdmin.cs
+++ HelpBoardUA/Controllers/DBforAdmin.cs
@@ -22,9 +22,21 @@ namespace HelpBoardUA.Controllers
 			_userManager = userManager;
 			_logger = logger;
 		}
-		public async Task<IActionResult> UsersDB()
+		public async Task<IActionResult> UsersDB(ClientViewModel? searchModel)
 		{
 			var list = await _appDbContext.Clients.ToListAsync();
+			
+
+			if (searchModel != null && !string.IsNullOrEmpty(searchModel.LastName))
+			{
+				// Если указано имя в поисковой модели, фильтруем список организаций по имени
+				list = await _appDbContext.Clients
+		.Where(u =>
+			(string.IsNullOrEmpty(searchModel.LastName) || u.FirstName.Contains(searchModel.LastName)) ||
+			(string.IsNullOrEmpty(searchModel.LastName) || u.LastName.Contains(searchModel.LastName)) ||
+			(string.IsNullOrEmpty(searchModel.LastName) || u.Patronymic.Contains(searchModel.LastName)))
+		.ToListAsync();
+			}
 
 			var model = new ClientViewModel()
 			{
@@ -34,40 +46,41 @@ namespace HelpBoardUA.Controllers
 			return View(model);
 		}
 
-		public async Task<IActionResult> OrganizationsDB(OrganizationViewModel model)
+		public async Task<IActionResult> OrganizationsDB(OrganizationViewModel searchModel)
 		{
 			var list = await _appDbContext.Organizations.ToListAsync();
 
-				model = new OrganizationViewModel()
+			if (searchModel != null)
+			{
+				// Фильтруем список организаций по имени
+				if (!string.IsNullOrEmpty(searchModel.Name))
 				{
-					OrganizationList = list
-				};
-			
-			return View(model);
-		}
-
-		/*public async Task<IActionResult> OrgFilter()
-		{
-            var list = await _appDbContext.Organizations.ToListAsync();
-
-            var model = new OrganizationViewModel()
-            {
-                OrganizationList = list
-            };
-
-            return View(model);
-        }*/
-
-		public async Task<IActionResult> OrgSearch(OrganizationViewModel organization)
-		{
-			var list = await _appDbContext.Organizations.Where(org => (org.Name == organization.Name)).ToListAsync();
+					list = list.Where(org => org.Name.Contains(searchModel.Name)).ToList();
+				}		
+			}
 
 			var model = new OrganizationViewModel()
 			{
 				OrganizationList = list
 			};
 
-			return RedirectToAction("OrganizationsDB", model);
+			return View(model);
+		}
+
+		public async Task<IActionResult> OrgSearch(OrganizationViewModel organization)
+		{			
+			return RedirectToAction("OrganizationsDB", organization);
+		}
+
+		public async Task<IActionResult> OrgFilter(OrganizationViewModel organization)
+		{
+					
+			return RedirectToAction("OrganizationsDB", organization);
+		}
+
+		public async Task<IActionResult> UserSearch(ClientViewModel client)
+		{
+			return RedirectToAction("UsersDB", client);
 		}
 	}
 }
